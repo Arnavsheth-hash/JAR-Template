071d0404000354c987070429a4f535eb81d77025
Arnav Sheth <arnavsheth.programming@gmail.com>
2024-09-15 08:20:14 -0700

    Initial Commit

---
+++ include/robot-config.h
@@ -0,0 +1,30 @@
+using namespace vex;
+
+extern brain Brain;
+
+//To set up a motor called LeftFront here, you'd use
+//extern motor LeftFront;
+extern motor rf;
+extern motor rm;
+extern motor rb;
+
+extern motor lf;
+extern motor lm;
+extern motor lb;
+
+extern motor intake;
+extern motor arm;
+
+extern controller controller1;
+
+extern digital_out clampA;
+extern digital_out clampB;
+
+extern digital_out pto;
+
+extern digital_out armSecond;
+//Add your devices below, and don't forget to do the same in robot-config.cpp:
+
+
+
+void  vexcodeInit( void );
---
+++ src/robot-config.cpp
@@ -0,0 +1,41 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen.
+brain  Brain;
+
+//The motor constructor takes motors as (port, ratio, reversed), so for example
+//motor LeftFront = motor(PORT1, ratio6_1, false);
+
+//Add your devices below, and don't forget to do the same in robot-config.h:
+controller controller1;
+
+motor rf = motor(PORT14, ratio6_1, false);
+motor rm = motor(PORT20, ratio6_1, false);
+motor rb = motor(PORT17, ratio6_1, false);
+
+motor lf = motor(PORT13, ratio6_1, true);
+motor lm = motor(PORT18, ratio6_1, true);
+motor lb = motor(PORT11, ratio6_1, true);
+
+motor intake = motor(PORT7, ratio18_1, false);
+
+motor arm = motor(PORT2, ratio36_1, true);
+
+digital_out clampA = digital_out(Brain.ThreeWirePort.H);
+digital_out clampB = digital_out(Brain.ThreeWirePort.G);
+
+digital_out pto = digital_out(Brain.ThreeWirePort.F);
+
+digital_out armSecond = digital_out(Brain.ThreeWirePort.E);
+
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
+
+// left  = 8 , 2, 1
+
+// right = 9 , 10 , 20
